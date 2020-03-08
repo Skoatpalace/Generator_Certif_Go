78cc4e3ca30247e55e60ad38248533eda0f04a47
Skoatpalace <richard.fages@gmail.com>
2020-03-08 10:36:18 +0100

    add test and logic

--- cert/cert.go
+++ cert/cert.go
@@ -2,8 +2,12 @@ package cert
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
+
+var MaxLengthCourse = 20
+var MaxLengthName = 30
 
 type Cert struct {
 	Course string
@@ -23,18 +27,65 @@ type Saver interface {
 
 func New(course, name, date string) (*Cert, error) {
 
-	c := course
-	n := name
-	d := date
-	
+	c, err := validateCourse(course)
+	if err != nil {
+		return nil, err
+	}
+	n, err := validateName(name)
+	if err != nil {
+		return nil, err
+	}
+	d, err := parseDate(date)
+	if err != nil {
+		return nil, err
+	}
+
 	cert := &Cert{
-		Course : c,
-		Name : n,
-		LabelTitle : fmt.Sprintf("%v Certificate - %v", c, n),
-		LabelCompletion : "Certificate of Completion", 
-		LabelPresented : "This Certificate is Presented To",
-		LabelParticipation : fmt.Sprintf("For participation in the %v",c),
-		LabelDate : fmt.Sprintf("%v", d),		
+		Course:             c,
+		Name:               n,
+		Date:               d,
+		LabelTitle:         fmt.Sprintf("%v Certificate - %v", c, n),
+		LabelCompletion:    "Certificate of Completion",
+		LabelPresented:     "This Certificate is Presented To",
+		LabelParticipation: fmt.Sprintf("For participation in the %v", c),
+		LabelDate:          fmt.Sprintf("%v", d.Format("02/01/2006")),
 	}
 	return cert, nil
+}
+
+func validateCourse(course string) (string, error) {
+	c, err := validateStr(course, MaxLengthCourse)
+	if err != nil {
+		return "", err
+	}
+	if !strings.HasSuffix(c, " course") {
+		c = c + " course"
+	}
+	return strings.ToTitle(c), nil
+}
+
+func validateStr(str string, maxLen int) (string, error) {
+	c := strings.TrimSpace(str)
+	if len(c) <= 0 {
+		return c, fmt.Errorf("Invalid string. got'%s', len=%d", c, len(c))
+	} else if len(c) >= maxLen {
+		return c, fmt.Errorf("Invalid string. got'%s', len=%d", c, len(c))
+	}
+	return c, nil
+}
+
+func validateName(name string) (string, error) {
+	n, err := validateStr(name, MaxLengthName)
+	if err != nil {
+		return "", err
+	}
+	return strings.ToTitle(n), nil
+}
+
+func parseDate(date string) (time.Time, error)  {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return t, err
+	}
+	return t, nil
 }
--- cert/cert_test.go
+++ cert/cert_test.go
@@ -5,5 +5,44 @@ import (
 )
 
 func TestValidCertData(t *testing.T) {
-	//c, err := New("Golang", "Bob", "2019-05-31")
+	c, err := New("Golang", "Bob", "2019-05-31")
+	if err != nil {
+		t.Errorf("Cert data should be valid. err = %v", err)
+	}
+	if c == nil {
+		t.Errorf("Cert should be a valid reference. got = nil")
+	}
+	if c.Course != "GOLANG COURSE" {
+		t.Errorf("Course name is not valid. Expected = 'GOLANG COURSE' got = %v", c.Course)
+	}
+}
+
+func TestCourseEmptyValue(t *testing.T)  {
+	_, err := New("", "Bob", "2019-05-31")
+	if err == nil {
+		t.Error("Error should be returned an empty course")
+	}
+}
+
+func TestNameEmptyValue(t *testing.T)  {
+	_, err := New("Golang", "", "2019-05-31")
+	if err == nil {
+		t.Error("Error should be returned an empty name")
+	}
+}
+
+func TestCourseTooLong(t *testing.T)  {
+	course := "azzzzerthgbfdqetryhtjhngvtreythrngbddrtrethgbfvdezgebdvcssrgfbvcxd"
+	_, err := New(course, "bob", "2019-05-31")
+	if err == nil {
+		t.Errorf("Error should be returned on a too long course name (course = %s)", course)
+	}
+}
+
+func TestNameTooLong(t *testing.T)  {
+	name := "azzzzerthgbfdqetryhtjhngvtreythrngbddrtrethgbfvdezgebdvcssrgfbvcxd"
+	_, err := New("Golang",name, "2019-05-31")
+	if err == nil {
+		t.Errorf("Error should be returned on a too long student name (name = %s)", name)
+	}
 }
